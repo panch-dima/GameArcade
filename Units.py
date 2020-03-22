653ab932ea06f25a6d21b754dc6841c43adcb33c
panchukda <panch-dima@yandex.ru>
2020-03-22 18:01:42 +0300

    init wolf

--- Units.py
+++ Units.py
@@ -4,5 +4,14 @@ class Player(arcade.Sprite):
     def __init__(self, filename, sprite_scaling):
         super(Player, self).__init__(filename, sprite_scaling)
         self.leave = 100
+
+    def update(self):
+        pass
+
+class Wolf(arcade.Sprite):
+    def __init__(self, filename, prite_scaling):
+        super(Player, self).__init__(filename, sprite_scaling)
+        self.leave = 100
+
     def update(self):
         pass
